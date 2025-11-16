a55c174b04af3bbcbc99326e0cdbe428d889b0e8
Md. Shahidul Islam <shanto57575@gmail.com>
2025-11-16 18:45:27 +0600

    activity fucntionality added

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import { envVariables } from "./config/envVariables.js";
 import { seedSuperAdmin } from "./utils/seedSuperAdmin.js";
 import app from "./app.js";
@@ -7,70 +7,69 @@ import { connectRedis } from "./config/redisClient.js";
 let server;
 
 const startServer = async () => {
-    try {
-        await mongoose.connect(envVariables.MONGO_URI)
+  try {
+    await mongoose.connect(envVariables.MONGO_URI);
 
-        console.log("Connected to DB ✅");
+    console.log("Connected to DB ✅");
 
-        server = app.listen(envVariables.PORT, () => {
-            console.log(`Server is listening to port ${envVariables.PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
+    server = app.listen(envVariables.PORT, () => {
+      console.log(`Server is listening to port ${envVariables.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 (async () => {
-    await connectRedis()
-    await startServer()
-    await seedSuperAdmin()
-})()
+  await connectRedis();
+  await startServer();
+  await seedSuperAdmin();
+})();
 
 process.on("SIGTERM", () => {
-    console.log("SIGTERM signal received... Server shutting down..");
+  console.log("SIGTERM signal received... Server shutting down..");
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
+  process.exit(1);
+});
 
 process.on("SIGINT", () => {
-    console.log("SIGINT signal received... Server shutting down..");
+  console.log("SIGINT signal received... Server shutting down..");
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
-
+  process.exit(1);
+});
 
 process.on("unhandledRejection", (err) => {
-    console.log("Unhandled Rejection detected... Server shutting down..", err);
+  console.log("Unhandled Rejection detected... Server shutting down..", err);
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
+  process.exit(1);
+});
 
 process.on("uncaughtException", (err) => {
-    console.log("Uncaught Exception detected... Server shutting down..", err);
+  console.log("Uncaught Exception detected... Server shutting down..", err);
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-})
+  process.exit(1);
+});
